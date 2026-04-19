db8e63edc632fde60ec3bc087fb9aa9448606234
Aramis Alexander <alexanderaec91@gmail.com>
2026-04-19 17:31:35 -0500

    Se agrego el metodo suma en la clase calculadora

---
+++ src/campo_semna_03/Calculadora.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package campo_semna_03;
+
+/**
+ *
+ * @author ALEXANDER
+ */
+public class Calculadora {
+    
+    public void sumar(int a, int b){
+        
+        int resultado = (a + b);
+        System.out.println("La suma es: " + resultado);
+    }
+    
+}
---
+++ src/campo_semna_03/Campo_Semna_03.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package campo_semna_03;
+
+/**
+ *
+ * @author ALEXANDER
+ */
+public class Campo_Semna_03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Calculadora c = new Calculadora();
+        
+        c.sumar(10, 10);
+    }
+    
+}
